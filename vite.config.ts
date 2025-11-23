ad621f2c4a36355197d329de3e90109857d7af5f
thughari <thughari3@gmail.com>
2025-11-23 21:44:59 +0530

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // Replace with your repo name (from package.json "name")
 export default defineConfig({
-  base: '/hari-thatikonda-portfolio/',
+  base: '/Portfolio/',
   plugins: [react()],
 });
